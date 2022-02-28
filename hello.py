829db49034b5f3dbddca150f3a9c6266b6e899e7
vardhinii <vardhini.kumaar@gmail.com>
2022-02-28 19:21:52 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,29 @@
+#Based on Kaggle tutorials
+
+#Variable-Assignment
+spam_amount = 0
+#Things to notice:
+# 1) no variable declaration like in C++
+# 2) no need to specify type & can be changed with other things
+
+#print function
+print(spam_amount)
+
+#Re-Assignment
+spam_amount = spam_amount + 4
+
+#Conditional-Statements
+if spam_amount > 0:
+    print ("I dont want ANY spam!")
+
+# : indicates new code block, no need of {}
+
+#Operators
+viking_song = "spam " * spam_amount
+print(viking_song)
+
+# * used to multiply strings to no.s and no.s to no.s
+
+#type function
+type(spam_amount)
+
